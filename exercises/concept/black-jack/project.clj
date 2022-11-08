10481d243fa0579047a7e63b23f84325132df93b
Mark Mestern <mark.mestern@uswitch.com>
2022-11-08 20:15:31 +0000

    Update project file

--- exercises/concept/black-jack/project.clj
+++ exercises/concept/black-jack/project.clj
@@ -1,4 +1,4 @@
-(defproject cars-assemble "0.1.0-SNAPSHOT"
-  :description "cars-assemble exercise."
-  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/cars-assemble"
+(defproject black-jack "0.1.0-SNAPSHOT"
+  :description "black-jack exercise."
+  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/black-jack"
   :dependencies [[org.clojure/clojure "1.10.0"]])
