789517f25ddbd922fae1ccb235e87fef9692746a
Blair Nangle <hi@blairnangle.com>
2022-11-08 20:14:03 +0000

    Add first test case (test-first-card)

--- exercises/concept/black-jack/test/black_jack_test.clj
+++ exercises/concept/black-jack/test/black_jack_test.clj
@@ -1,51 +1,21 @@
-(ns cars-assemble-test
+(ns black-jack-test
   (:require [clojure.test :refer [deftest testing is]]
             cars-assemble))
 
-(deftest production-rate-speed-0-test
-    (testing "Production rate for speed 0"
-      (is (= 0.0 (cars-assemble/production-rate 0)))))
+; copied from https://exercism.org/tracks/python/exercises/black-jack
+; functions to test:
+; value-of-card
+; higher-card
+; is-blackjack
+; can-split_pairs
+; can-double_down
 
-(deftest production-rate-speed-1-test
-    (testing "Production rate for speed 1"
-      (is (= 221.0 (cars-assemble/production-rate 1)))))
-
-(deftest production-rate-speed-4-test
-    (testing "Production rate for speed 4"
-      (is (= 884.0 (cars-assemble/production-rate 4)))))
-
-(deftest production-rate-speeed-7-test
-    (testing "Production rate for speed 7"
-      (is (= 1392.3 (cars-assemble/production-rate 7)))))
-
-(deftest production-rate-speed-9-test
-    (testing "Production rate for speed 9"
-      (is (= 1591.2 (cars-assemble/production-rate 9)))))
-
-(deftest production-rate-speed-10-test
-    (testing "Production rate for speed 10"
-      (is (= 1701.7 (cars-assemble/production-rate 10)))))
-
-(deftest working-items-speed-0-test
-    (testing "Working items for speed 0"
-      (is (= 0 (cars-assemble/working-items 0)))))
-
-(deftest working-items-speed-1-test
-    (testing "Working items for speed 1"
-      (is (= 3 (cars-assemble/working-items 1)))))
-
-(deftest working-items-speed-5-test
-    (testing "Working items for speed 5"
-      (is (= 16 (cars-assemble/working-items 5)))))
-
-(deftest working-items-speed-8-test
-    (testing "Working items for speed 8"
-      (is (= 26 (cars-assemble/working-items 8)))))
-
-(deftest working-items-speed-9-test
-    (testing "Working items for speed 9"
-      (is (= 26 (cars-assemble/working-items 9)))))
-
-(deftest working-items-speed-10-test
-    (testing "Working items for speed 10"
-      (is (= 28 (cars-assemble/working-items 10)))))
+(deftest test-value-of-card
+  (is (= 2 (black-jack/value-of-card "2")))
+  (is (= 5 (black-jack/value-of-card "5")))
+  (is (= 8 (black-jack/value-of-card "8")))
+  (is (= 1 (black-jack/value-of-card "A")))
+  (is (= 10 (black-jack/value-of-card "10")))
+  (is (= 10 (black-jack/value-of-card "J")))
+  (is (= 10 (black-jack/value-of-card "Q")))
+  (is (= 10 (black-jack/value-of-card "K"))))
