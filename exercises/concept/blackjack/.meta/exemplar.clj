4591d511df9d21393ad5a1bc629b3c9d897cea3f
Mark Mestern <mark.mestern@uswitch.com>
2022-11-08 20:49:56 +0000

    Complete exemplar for parse card

--- exercises/concept/blackjack/.meta/exemplar.clj
+++ exercises/concept/blackjack/.meta/exemplar.clj
@@ -2,12 +2,20 @@
 
 ;; ParseCard returns the integer value of a card following blackjack ruleset.
 (defn parse-card [card]
-  {"ace" 11
-   "two" 2
-   "three" 3
-   "joker" 0}
-  )
-
+  ({"ace"   11
+    "two"   2
+    "three" 3
+    "four"  4
+    "five"  5
+    "six"   6
+    "seven" 7
+    "eight" 8
+    "nine"  9
+    "ten"   10
+    "jack"  10
+    "queen" 10
+    "king"  10
+    "joker" 0} card))
 
 ;; FirstTurn returns the decision for the first turn, given two cards of the
 ;; player and one card of the dealer.
